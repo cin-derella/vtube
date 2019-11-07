de64aa7e28dcbcebf87d79c2b9993c39a1ceb88e
cin-derella <shirley_cinderella@outlook.com>
2019-11-07 21:15:17 +0800

    check the type of the upload

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -2,6 +2,7 @@
     class VideoProcessor{
         private $con;
         private $sizeLimit = 500000000;
+        private $allowedTypes = array("mp4","flv","webm","mkv","vob","ogv","avi","wmv","mov","mpeg","mpg");
 
         public function __construct($con){
             $this->con = $con;
@@ -16,8 +17,6 @@
 
             $isValidData = $this->processData($videoData,$tempFilePath);
 
-
-            echo $tempFilePath;
         }
 
         private function processData($videoData,$filePath){
@@ -27,10 +26,20 @@
                 echo "File too large.Can't be more than".$this->sizeLimit."bytes";
                 return false;
             }
+            else if (!$this->isValidType($videoType)){
+                echo "Invalid file type";
+                return false;
+            }
         }
 
         private function isValidSize($data){
             return $data["size"]<=$this->sizeLimit;
+        }
+
+        private function isValidType($type){
+            $lowercased = strtolower($type);
+            return in_array($lowercased,$this->allowedTypes);
+
         }
 
     }
