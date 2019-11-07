68805822229240772bbd93b510e9751290ca0846
cin-derella <shirley_cinderella@outlook.com>
2019-11-07 21:06:22 +0800

    check the size of the upload

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -1,6 +1,8 @@
 <?php 
     class VideoProcessor{
         private $con;
+        private $sizeLimit = 500000000;
+
         public function __construct($con){
             $this->con = $con;
         }
@@ -12,7 +14,23 @@
             //"uploads/videos/5dc045126f608cats_playing.flv
             $tempFilePath = str_replace(" ","",$tempFilePath);
 
+            $isValidData = $this->processData($videoData,$tempFilePath);
+
+
             echo $tempFilePath;
+        }
+
+        private function processData($videoData,$filePath){
+            $videoType = pathInfo($filePath,PATHINFO_EXTENSION);
+
+            if(!$this->isValidSize($videoData)){
+                echo "File too large.Can't be more than".$this->sizeLimit."bytes";
+                return false;
+            }
+        }
+
+        private function isValidSize($data){
+            return $data["size"]<=$this->sizeLimit;
         }
 
     }
