9dcf4cde24c8bf6a0b802be4c49b348b13b21c08
cin-derella <shirley_cinderella@outlook.com>
2019-11-10 22:17:17 +0800

    validate email address

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -12,6 +12,8 @@ class Account{
     public function register($fn,$ln,$un,$em,$em2,$pw,$pw2){
         $this->validateFirstName($fn);
         $this->validateLastName($ln);
+        $this->validateusername($un);
+        $this->validateEmails($em,$em2);
     }
 
     private function validateFirstName($fn){
@@ -23,6 +25,12 @@ class Account{
     private function validateLastName($ln){
         if(strlen($ln)>25 || strlen($ln)<2){
             array_push($this->errorArray,Constants::$lastNameCharacters);
+        }
+    }
+
+    private function validateUsername($un){
+        if(strlen($un)>25 || strlen($un)<5){
+            array_push($this->errorArray,Constants::$usernameCharacters);
             return;
         }
         $query = $this->con->prepare("SELECT username FROM users WHERE username=:un");
@@ -32,16 +40,27 @@ class Account{
         if($query->rowCount()!=0){
             array_push($this->errorArray,Constants::$usernameTaken);
         }
-
-
     }
 
-    private function validateUsername($un){
-        if(strlen($un)>25 || strlen($un)<5){
-            array_push($this->errorArray,Constants::$usernameCharacters);
+    private function validateEmails($em,$em2){
+        if($em != $em2){
+            array_push($this->errorArray,Constants::$emailsDoNotMatch);
+            return;
+        }
+
+        if(!filter_var($em,FILTER_VALIDATE_EMAIL)){
+            array_push($this->errorArray,Constants::$emailInvalid);
+            return;
+
+        }
+        $query = $this->con->prepare("SELECT email FROM users WHERE email=:em");
+        $query->bindParam(":em",$em);
+        $query->execute();
+
+        if($query->rowCount()!=0){
+            array_push($this->errorArray,Constants::$emailTaken);
         }
     }
-
     public function getError($error){
         if(in_array($error,$this->errorArray)){
             return "<span class = 'errorMessage'>$error</span>";
