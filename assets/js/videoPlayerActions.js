abfe372c93b23a01369c838e614f7d7e2a040c98
cin-derella <shirley_cinderella@outlook.com>
2019-11-19 00:39:42 +0800

    dislike function

--- assets/js/videoPlayerActions.js
+++ assets/js/videoPlayerActions.js
@@ -13,6 +13,46 @@ function likeVideo(button,videoId){
         var result = JSON.parse(data);
         updateLikesValue(likeButton.find(".text"),result.likes);
         updateLikesValue(dislikeButton.find(".text"),result.dislikes);
+
+        if(result.likes<0){
+            likeButton.removeClass("active");
+            likeButton.find("img:first").attr("src","assets/images/icons/thumb-up.png");
+        }
+        else{
+            likeButton.find("img:first").attr("src","assets/images/icons/thumb-up-active.png")
+        }
+
+        dislikeButton.find("img:first").attr("src","assets/images/icons/thumb-down.png");
+    });
+}
+
+
+function dislikeVideo(button,videoId){
+    console.log(`dislike Video ${videoId}`);
+    $.post("ajax/dislikeVideo.php",{videoId:videoId})
+    .done(function(data){
+
+        var dislikeButton = $(button);
+        var likeButton = $(button).siblings(".likeButton");
+
+        dislikeButton.addClass("active");
+        likeButton.removeClass("active");
+
+        console.log(`data is [${data}]`);
+
+        var result = JSON.parse(data);
+        updateLikesValue(likeButton.find(".text"),result.likes);
+        updateLikesValue(dislikeButton.find(".text"),result.dislikes);
+
+        if(result.dislikes<0){
+            dislikeButton.removeClass("active");
+            dislikeButton.find("img:first").attr("src","assets/images/icons/thumb-down.png");
+        }
+        else{
+            dislikeButton.find("img:first").attr("src","assets/images/icons/thumb-down-active.png")
+        }
+
+        likeButton.find("img:first").attr("src","assets/images/icons/thumb-up.png");
     });
 }
 
