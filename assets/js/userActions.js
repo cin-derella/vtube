af97d1fe12fa889c02c285b472b1e52923cc3be5
cin-derella <shirley_cinderella@outlook.com>
2019-11-23 00:59:20 +0800

    pass values into the subscribe function

--- assets/js/userActions.js
+++ assets/js/userActions.js
@@ -4,10 +4,9 @@ function subscribe(userTo,userFrom,button){
         return;
     }
 
-    $.post("ajax/subscribe.php")
-    .done(function(){
-        
-        console.log("done");
+    $.post("ajax/subscribe.php",{userTo: userTo,userFrom: userFrom})
+    .done(function(data){
+        console.log(data);
     });
 
 }
