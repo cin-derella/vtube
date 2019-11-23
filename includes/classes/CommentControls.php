948c5f616fd913f51b38fcb92d16205e1e0ba0f0
cin-derella <shirley_cinderella@outlook.com>
2019-11-23 17:25:27 +0800

    create the comment likes and dislikes buttons

--- includes/classes/CommentControls.php
+++ includes/classes/CommentControls.php
@@ -32,7 +32,7 @@ class CommentControls{
         return ButtonProvider::createButton($text,null,$action,null);
     }
 
-    private function createlikeButton(){
+    private function createlikeCount(){
         $text = $this->comment->getLikes();
         if($text == 0){
             $text = "";
@@ -46,34 +46,35 @@ class CommentControls{
 
 
     private function createLikeButton(){
-        $text = $this->video->getLikes();
-        $videoId = $this->video->getId();
-        $action = "likeVideo(this,$videoId)";
+    
+        $videoId = $this->comment->getVideoId();
+        $commentId = $this->comment->getId();
+        $action = "likeComment($commentId,this,$videoId)";
         $class = "likeButton";
 
         $imageSrc = "assets/images/icons/thumb-up.png";
 
-        if($this->video->wasLikedBy()){
+        if($this->comment->wasLikedBy()){
             $imageSrc = "assets/images/icons/thumb-up-active.png";
         }
 
-        return ButtonProvider::createButton($text,$imageSrc,$action,$class);
+        return ButtonProvider::createButton("",$imageSrc,$action,$class);
      
     }
 
     private function createDislikeButton(){
-        $text = $this->video->getDislikes();
-        $videoId = $this->video->getId();
-        $action = "dislikeVideo(this,$videoId)";
+        $commentId = $this->comment->getId();
+        $videoId = $this->comment->getVideoId();
+        $action = "dislikeComment($commentId,this,$videoId)";
         $class = "dislikeButton";
 
         $imageSrc = "assets/images/icons/thumb-down.png";
 
-        if($this->video->wasDislikedBy()){
+        if($this->comment->wasDislikedBy()){
             $imageSrc = "assets/images/icons/thumb-down-active.png";
         }
 
-        return ButtonProvider::createButton($text,$imageSrc,$action,$class);
+        return ButtonProvider::createButton("",$imageSrc,$action,$class);
      
     }
 }
