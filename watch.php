a45c2e1547d58abe870b50f44d38c2dcc8445ac2
cin-derella <shirley_cinderella@outlook.com>
2019-11-24 15:40:57 +0800

    create the video grid class

--- watch.php
+++ watch.php
@@ -34,7 +34,10 @@ $video->incrementViews();
 </div>
 
 <div class="suggestions">
-
+   <?php
+    //    $videoGrid = new VideoGrid($con,$userLoggedInObj);
+      //  echo $videoGrid->create(null,null,false);
+    ?>
 
 </div>
 
