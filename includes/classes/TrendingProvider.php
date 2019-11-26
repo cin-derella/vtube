9751460816a86bb4b104a92d4c0a0228517be956
cin-derella <shirley_cinderella@outlook.com>
2019-11-26 21:42:19 +0800

    trending page

--- includes/classes/TrendingProvider.php
+++ includes/classes/TrendingProvider.php
@@ -4,18 +4,19 @@ class TrendingProvider{
         public function __construct($con,$userLoggedInObj){
             $this->con = $con;
             $this->userLoggedInObj = $userLoggedInObj;
-}
+        }
 
-public funct getVideos(){
-    $videos = array();
-    $query= $this->con->prepare("SELECT * FROM uploadDate WHERE uploadDate>= now() -INTERVAL 7 DAY
-                                ORDER BY views DESC LIMIT 15");
-    $query->execute();
+    public function getVideos(){
+        $videos = array();
+        $query= $this->con->prepare("SELECT * FROM videos WHERE uploadDate>= now() -INTERVAL 7 DAY
+                                    ORDER BY views DESC LIMIT 15");
+        $query->execute();
 
-    while($row =$query->fetch(PDO::FETCH_ASSOC)){
-        $video = new Video($this->con, $row,$this->userLoggedInObj);
-        array_push($videos,$video);
+        while($row =$query->fetch(PDO::FETCH_ASSOC)){
+            $video = new Video($this->con, $row,$this->userLoggedInObj);
+            array_push($videos,$video);
+        }
+        return $videos;
     }
-    return videos;
 }
 ?>
