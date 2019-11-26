30527f2fc6824df6e60ac16871d160c192b465b6
cin-derella <shirley_cinderella@outlook.com>
2019-11-26 21:55:44 +0800

    liked video page

--- includes/classes/NavigationMenuProvider.php
+++ includes/classes/NavigationMenuProvider.php
@@ -10,7 +10,7 @@ class NavigationMenuProvider{
         $menuHtml = $this->createNavItem("Home","assets/images/icons/home.png","index.php");
         $menuHtml .= $this->createNavItem("Trending","assets/images/icons/trending.png","trending.php");
         $menuHtml .= $this->createNavItem("Subscriptions","assets/images/icons/subscriptions.png","subscriptions.php");
-        $menuHtml .= $this->createNavItem("Liked Videos","assets/images/icons/thumb-up.png","likeVideos.php");
+        $menuHtml .= $this->createNavItem("Liked Videos","assets/images/icons/thumb-up.png","likedVideos.php");
    
         if(User::isLoggedIn()){
             $menuHtml .= $this->createNavItem("Settings","assets/images/icons/settings.png","settings.php");
