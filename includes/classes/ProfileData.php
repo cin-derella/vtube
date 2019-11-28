4f190baa0426ef1b3b5c56ec1a0d53770dc005a2
cin-derella <shirley_cinderella@outlook.com>
2019-11-28 22:49:06 +0800

    output the cover photo

--- includes/classes/ProfileData.php
+++ includes/classes/ProfileData.php
@@ -23,5 +23,13 @@ class ProfileData{
 
         return "assets/images/coverPhotos/default-cover-photo.jpg";
     }
+
+    public function getProfileUserFullName(){
+
+        return $this->profileUserObj->getName();
+    }
+
+    
+
 }
 ?>
