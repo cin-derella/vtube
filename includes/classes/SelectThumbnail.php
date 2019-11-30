a6b2d4b1d9b173244dafd85ae9eb7efc5d1b74a6
cin-derella <shirley_cinderella@outlook.com>
2019-11-30 14:57:45 +0800

    get the video thumbnail

---
+++ includes/classes/SelectThumbnail.php
@@ -0,0 +1,28 @@
+<?php
+class SelectThumbnail{
+    private $con,$video;
+
+    public function __construct($con,$video){
+        $this->con = $con;
+        $this->video = $video;
+    }
+
+    public function create(){
+        $thumbnailData = $this->getThumbnailData();
+    }
+
+    private function getThumbnailData(){
+        $data = array();
+        $query=$this->con->prepare("SELECT * FROM thumbnails WHERE videoId=:videoId");
+        $query->bindParam(":videoId",$videoId);
+        $videoId = $this->video->getId();
+
+        $query->execute();
+
+        while($row=$query->fetch(PDO::FETCH_ASSOC)){
+            $data[] = $row;
+        }
+        return $data;
+    }
+}
+?>
