b79e234b9418d736b5cbe0f293fcf56ad69fafcc
cin-derella <shirley_cinderella@outlook.com>
2019-11-30 17:08:29 +0800

    update the video details

--- editVideo.php
+++ editVideo.php
@@ -19,12 +19,43 @@ if($video->getUploadedBy()!=$userLoggedInObj->getUsername()){
     echo "Not your video";
     exit();
 }
+
+$detailsMessage = "";
+
+if(isset($_POST["saveButton"])){
+    $videoData = new VideoUploadData(
+        null,
+        $_POST["titleInput"],
+        $_POST["descriptionInput"],
+        $_POST["privacyInput"],
+        $_POST["categoryInput"],
+        $userLoggedInObj->getUsername()
+    );
+
+    if($videoData->updateDetails($con,$video->getId())){
+        $detailsMessage = "<div class = 'alert alert-success'>
+                                <strong>SUCCESS! </strong>Details updated successfully!
+                            </div>";
+        $video = new Video($con,$_GET["videoId"],$userLoggedInObj);
+    }
+    else{
+      
+        $detailsMessage = "<div class = 'alert alert-danger'>
+                                <strong>ERROR! </strong>Something went wrong
+                            </div>";
+    }
+}
 ?>
 
 <script src = "assets/js/editVideoActions.js"></script>
 
 <div class="editVideoContainer column">
-    <div class="topSection">
+
+    <div class="message">
+        <?php echo $detailsMessage; ?>        
+
+    </div>
+<div class="topSection">
         <?php
         $videoPlayer = new VideoPlayer($video);
         echo $videoPlayer->create(false);
